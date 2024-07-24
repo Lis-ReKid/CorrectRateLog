98cb120181e78daa8f881015f58a49ef4c75e327
Lis0715 <s.yamamoto.ad98@gmail.com>
2024-07-24 17:18:12 +0900

    履歴新規登録画面を作成 ホーム画面から履歴新規登録画面への画面遷移を追加

--- app/src/main/java/com/re_kid/lis/accuracychecker/NoDataFragment.java
+++ app/src/main/java/com/re_kid/lis/accuracychecker/NoDataFragment.java
@@ -1,5 +1,6 @@
 package com.re_kid.lis.accuracychecker;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,8 +62,17 @@ public class NoDataFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-    }
 
+        Button btnShowCreateHistoryView = view.findViewById(R.id.btn_show_create_history_view);
+        btnShowCreateHistoryView.setOnClickListener(new BtnClickListener());
+    }
+    private class BtnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getActivity(), CreateHistoryActivity.class);
+            startActivity(intent);
+        }
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
