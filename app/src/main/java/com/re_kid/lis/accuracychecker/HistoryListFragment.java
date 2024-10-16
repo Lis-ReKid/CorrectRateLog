4be8fb79042b0f0575fa51e235472085e6f657e7
Lis0715 <s.yamamoto.ad98@gmail.com>
2024-10-16 14:20:44 +0900

    仮リスト表示できるようにした

--- app/src/main/java/com/re_kid/lis/accuracychecker/HistoryListFragment.java
+++ app/src/main/java/com/re_kid/lis/accuracychecker/HistoryListFragment.java
@@ -1,63 +1,55 @@
 package com.re_kid.lis.accuracychecker;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link HistoryListFragment#newInstance} factory method to
- * create an instance of this fragment.
  *
  */
 public class HistoryListFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HistoryListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HistoryListFragment newInstance(String param1, String param2) {
-        HistoryListFragment fragment = new HistoryListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     public HistoryListFragment() {
-        // Required empty public constructor
+        super(R.layout.fragment_history_list);
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_history_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ListView lvHistory = view.findViewById(R.id.lvHistory);
+        //テスト用仮リスト
+        List<Map<String, Integer>> tempList = new ArrayList<>();
+        Map<String, Integer> tempMap = new HashMap<>();
+        for (int i = 1; i <= 10; i++) {
+            tempMap.put("temp", i);
+            tempList.add(tempMap);
+        }
+        String[] from = {};
+        int[] to = {};
+        SimpleAdapter adapter = new SimpleAdapter(getActivity(), tempList, R.layout.history_row, from, to);
+        lvHistory.setAdapter(adapter);
     }
 }
