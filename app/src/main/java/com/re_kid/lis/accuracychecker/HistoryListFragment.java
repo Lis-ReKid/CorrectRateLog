461b08336daff070a98ddd43509065134e1ab0d3
Lis0715 <s.yamamoto.ad98@gmail.com>
2024-11-13 17:10:29 +0900

    全件取得してそのまま表示

--- app/src/main/java/com/re_kid/lis/accuracychecker/HistoryListFragment.java
+++ app/src/main/java/com/re_kid/lis/accuracychecker/HistoryListFragment.java
@@ -1,5 +1,7 @@
 package com.re_kid.lis.accuracychecker;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,21 +10,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import android.widget.SimpleCursorAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  *
  */
 public class HistoryListFragment extends Fragment {
+    private DatabaseHelper _helper;
     public HistoryListFragment() {
         super(R.layout.fragment_history_list);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        _helper = new DatabaseHelper(getActivity());
     }
 
     @Override
@@ -37,17 +42,18 @@ public class HistoryListFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         ListView lvHistory = view.findViewById(R.id.lvHistory);
-        //テスト用仮リスト
-        List<Map<String, Object>> tempList = new ArrayList<>();
-        Map<String, Object> tempMap = new HashMap<>();
-        for (int i = 1; i <= 10; i++) {
-            tempMap.put("temp", i);
-            tempList.add(tempMap);
-        }
-        String[] from = {};
-        int[] to = {};
-        //仮リストここまで
-        SimpleAdapter adapter = new SimpleAdapter(getActivity(), tempList, R.layout.history_row, from, to);
+        SQLiteDatabase db = _helper.getWritableDatabase();
+        String sql = "SELECT * FROM Histories ORDER BY _id DESC";
+        Cursor cursor = db.rawQuery(sql, null);
+        String[] from = {"history_datetime", "accuracy_rate", "accurate_number", "entire_number"};
+        int[] to = {R.id.tvHistoryDateTimeRow, R.id.tv_accuracy_rate_row, R.id.tv_accuracy_number_row, R.id.tv_entire_number_row};
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(getActivity(), R.layout.history_row, cursor, from, to, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
         lvHistory.setAdapter(adapter);
+    }
+
+    @Override
+    public void onDestroy() {
+        _helper.close();
+        super.onDestroy();
     }
 }
