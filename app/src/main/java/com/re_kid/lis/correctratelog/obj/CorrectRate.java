64b60efc3d7faedb3fa1131d8afda778d3628c97
Lis0715 <s.yamamoto.ad98@gmail.com>
2024-11-26 13:26:11 +0900

    コンストラクタの引数をStringに変更

--- app/src/main/java/com/re_kid/lis/correctratelog/obj/CorrectRate.java
+++ app/src/main/java/com/re_kid/lis/correctratelog/obj/CorrectRate.java
@@ -7,21 +7,25 @@ public class CorrectRate {
     private final int correctNum;
     private final int entireNum;
     private final double correctRate;
-    public CorrectRate(final int correctNum, final int entireNum) {
+    public CorrectRate(final String correctNum, final String entireNum) {
+        final int intCorrect = Integer.parseInt(correctNum);
+        final int intEntire = Integer.parseInt(entireNum);
         // 正答数0未満でエラー
-        if (correctNum < 0) throw new IllegalArgumentException("正答数は0以上の数値を指定してください。");
+        if (intCorrect < 0) throw new IllegalArgumentException("正答数は0以上の数値を指定してください。");
         // 問題数1未満でエラー
-        if (entireNum < 1) throw new IllegalArgumentException("問題数は1以上の数値を指定してください。");
+        if (intEntire < 1) throw new IllegalArgumentException("問題数は1以上の数値を指定してください。");
         // 正答数>問題数でエラー
-        if (correctNum > entireNum) throw new IllegalArgumentException("正答数が問題数を超えています。");
+        if (intCorrect > intEntire) throw new IllegalArgumentException("正答数が問題数を超えています。");
 
         this.correctNum = correctNum;
         this.entireNum = entireNum;
+        this.correctNum = intCorrect;
+        this.entireNum = intEntire;
 
         // 正答率を初期化
         // 小数点以下3桁のdouble型
-        BigDecimal decDividend = new BigDecimal(correctNum);
-        BigDecimal decDivisor = new BigDecimal(entireNum);
+        BigDecimal decDividend = new BigDecimal(intCorrect);
+        BigDecimal decDivisor = new BigDecimal(intEntire);
         this.correctRate =  decDividend.divide(decDivisor, 3, RoundingMode.HALF_UP).doubleValue();
     }
     /**
