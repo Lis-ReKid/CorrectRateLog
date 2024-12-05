f0e363188035536838937df28358acf5853affa4
Lis0715 <s.yamamoto.ad98@gmail.com>
2024-12-05 14:06:34 +0900

    Modelクラス作りかけ

--- app/src/main/java/com/re_kid/lis/correctratelog/model/HistoriesModel.java
+++ app/src/main/java/com/re_kid/lis/correctratelog/model/HistoriesModel.java
@@ -1,4 +1,25 @@
 package com.re_kid.lis.correctratelog.model;
 
+import android.content.Context;
+
+import com.re_kid.lis.correctratelog.DatabaseHelper;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class HistoriesModel {
+    DatabaseHelper _helper;
+
+    public HistoriesModel(Context context) {
+        _helper = new DatabaseHelper(context);
+    }
+    public void createHistory(final String learnedDateTime,
+                              final int correctNum,
+                              final int entireNum,
+                              final double correctRate) {
+    }
+    public Map<String, String> getHistories (List<String> columns) {
+        return new HashMap<>();
+    }
 }
