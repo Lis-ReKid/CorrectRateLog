c2e642494ec3c63c079614e6108d885f35a24659
Lis0715 <s.yamamoto.ad98@gmail.com>
2024-12-09 15:49:24 +0900

    履歴件数でホーム画面のフラグメントを分岐

--- app/src/main/java/com/re_kid/lis/correctratelog/MainActivity.java
+++ app/src/main/java/com/re_kid/lis/correctratelog/MainActivity.java
@@ -1,15 +1,19 @@
 package com.re_kid.lis.correctratelog;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.re_kid.lis.correctratelog.model.HistoryModel;
 
 public class MainActivity extends AppCompatActivity {
-    private DatabaseHelper _helper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +24,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        _helper = new DatabaseHelper(MainActivity.this);
+
+        // フラグメントの取得
+        try(HistoryModel model = new HistoryModel(MainActivity.this)) {
+            int count = model.selectAll().getCount();
+            FragmentManager manager = getSupportFragmentManager();
+            FragmentTransaction transaction = manager.beginTransaction();
+            transaction.setReorderingAllowed(true);
+            if (count == 0) {
+                transaction.replace(R.id.top_fragment_container, NoDataFragment.class, null);
+            } else {
+                transaction.replace(R.id.top_fragment_container, CorrectRateFragment.class, null);
+                transaction.replace(R.id.bottom_fragment_container, HistoryListFragment.class, null);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
     protected void onDestroy() {
-        _helper.close();
         super.onDestroy();
     }
 }
