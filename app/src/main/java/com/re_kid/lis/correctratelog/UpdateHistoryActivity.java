3b7e67003202e3fdc1624cdda1bceef7d0dcc3c3
Lis0715 <s.yamamoto.ad98@gmail.com>
2025-01-11 10:58:55 +0900

    日時の入力フォームを追加

--- app/src/main/java/com/re_kid/lis/correctratelog/UpdateHistoryActivity.java
+++ app/src/main/java/com/re_kid/lis/correctratelog/UpdateHistoryActivity.java
@@ -1,8 +1,12 @@
 package com.re_kid.lis.correctratelog;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +14,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.re_kid.lis.correctratelog.dialog.DatePickerDialogFragment;
+import com.re_kid.lis.correctratelog.dialog.TimePickerDialogFragment;
 import com.re_kid.lis.correctratelog.obj.History;
+import com.re_kid.lis.correctratelog.obj.LearnedDate;
+import com.re_kid.lis.correctratelog.obj.LearnedTime;
 
-public class UpdateHistoryActivity extends AppCompatActivity {
+public class UpdateHistoryActivity extends AppCompatActivity
+        implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +50,38 @@ public class UpdateHistoryActivity extends AppCompatActivity {
         EditText etEntireNumber = findViewById(R.id.et_entire_number);
         etEntireNumber.setText(String.valueOf(history.getEntireNum()));
         System.out.println(history.getId());
+
+        // 日付ボタン押下時処理
+        tvLearnedDate.setOnClickListener(v -> {
+            var datePicker = new DatePickerDialogFragment();
+            var args = new Bundle();
+            args.putString("Date", tvLearnedDate.getText().toString());
+            datePicker.setArguments(args);
+            datePicker.show(getSupportFragmentManager(), "datePicker");
+        });
+
+        // 時間ボタン押下時処理
+        tvLearnedTime.setOnClickListener(v -> {
+            var timePicker = new TimePickerDialogFragment();
+            var args = new Bundle();
+            args.putString("Time", tvLearnedTime.getText().toString());
+            timePicker.setArguments(args);
+            timePicker.show(getSupportFragmentManager(), "timePicker");
+        });
     }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        TextView tvLearnedDate = findViewById(R.id.tv_learned_date);
+        var date = LearnedDate.of(year, month + 1, dayOfMonth);
+        tvLearnedDate.setText(date.toString());
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        TextView tvLearnedTime = findViewById(R.id.tv_learned_time);
+        var time = LearnedTime.of(hourOfDay, minute);
+        tvLearnedTime.setText(time.toString());
+    }
+
 }
