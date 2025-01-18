60a6f41e7ee7b4935e9e970a36098510d1f2a5bd
Lis0715 <s.yamamoto.ad98@gmail.com>
2025-01-18 11:28:12 +0900

    カテゴリリスト表示処理を追加

--- app/src/main/java/com/re_kid/lis/correctratelog/CategoryListActivity.java
+++ app/src/main/java/com/re_kid/lis/correctratelog/CategoryListActivity.java
@@ -1,12 +1,19 @@
 package com.re_kid.lis.correctratelog;
 
+import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.re_kid.lis.correctratelog.model.CategoryModel;
 
 public class CategoryListActivity extends AppCompatActivity {
 
@@ -20,5 +27,24 @@ public class CategoryListActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // カテゴリリストを作成
+        ListView lvCategory = findViewById(R.id.lvCategoryList);
+        String[] from = {"_id", "category_name"};
+        int[] to = {R.id.tvCategoryIdRow, R.id.tvCategoryNameRow};
+        Context context = CategoryListActivity.this;
+        Cursor cursor;
+        try (var model = new CategoryModel(context)) {
+            cursor = model.selectAll();
+            var adapter = new SimpleCursorAdapter(CategoryListActivity.this,
+                    R.layout.category_row,
+                    cursor,
+                    from,
+                    to,
+                    CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+            lvCategory.setAdapter(adapter);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
