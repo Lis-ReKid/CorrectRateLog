76d3a2f162b6a3e26714a1f174911c8c0dc6fe50
Lis0715 <s.yamamoto.ad98@gmail.com>
2025-01-21 11:02:57 +0900

    カテゴリをDBに登録

--- app/src/main/java/com/re_kid/lis/correctratelog/dialog/CreateCategoryConfirmDialogFragment.java
+++ app/src/main/java/com/re_kid/lis/correctratelog/dialog/CreateCategoryConfirmDialogFragment.java
@@ -10,16 +10,19 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
 import com.re_kid.lis.correctratelog.R;
+import com.re_kid.lis.correctratelog.model.CategoryModel;
+import com.re_kid.lis.correctratelog.obj.Category;
 
 public class CreateCategoryConfirmDialogFragment extends DialogFragment {
+    String _categoryName;
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        var categoryName = requireArguments().getString("categoryName");
+        _categoryName = requireArguments().getString("categoryName");
         var builder = new AlertDialog.Builder(getActivity());
         var listener = new ButtonClickListener();
         builder.setTitle(R.string.dialog_create_category_confirm_msg)
-                .setMessage(categoryName)
+                .setMessage(_categoryName)
                 .setPositiveButton(R.string.btn_create, listener)
                 .setNeutralButton(R.string.btn_cancel, listener);
         return builder.create();
@@ -28,7 +31,13 @@ public class CreateCategoryConfirmDialogFragment extends DialogFragment {
     private class ButtonClickListener implements DialogInterface.OnClickListener {
         @Override
         public void onClick(DialogInterface dialog, int which) {
-
+            if (which == Dialog.BUTTON_POSITIVE) {
+                try (var model = new CategoryModel(getActivity())) {
+                    model.create(new Category(0, _categoryName));
+                } catch (Exception e) {
+                    return;
+                }
+            }
         }
     }
 }
