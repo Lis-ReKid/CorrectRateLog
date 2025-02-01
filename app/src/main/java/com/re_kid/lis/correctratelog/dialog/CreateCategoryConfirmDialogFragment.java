fc0072f4f48b9c288146f450324e6a9fdfb4e3d2
Lis0715 <s.yamamoto.ad98@gmail.com>
2025-02-01 11:04:27 +0900

    実行ファイルを作成

---
+++ app/src/main/java/com/re_kid/lis/correctratelog/dialog/CreateCategoryConfirmDialogFragment.java
@@ -0,0 +1,47 @@
+package com.re_kid.lis.correctratelog.dialog;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+import com.re_kid.lis.correctratelog.R;
+import com.re_kid.lis.correctratelog.model.CategoryModel;
+import com.re_kid.lis.correctratelog.obj.Category;
+
+public class CreateCategoryConfirmDialogFragment extends DialogFragment {
+    String _categoryName;
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        _categoryName = requireArguments().getString("categoryName");
+        var builder = new AlertDialog.Builder(getActivity());
+        var listener = new ButtonClickListener();
+        builder.setTitle(R.string.dialog_create_category_confirm_msg)
+                .setMessage(_categoryName)
+                .setPositiveButton(R.string.btn_create, listener)
+                .setNeutralButton(R.string.btn_cancel, listener);
+        return builder.create();
+    }
+
+    private class ButtonClickListener implements DialogInterface.OnClickListener {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            if (which == Dialog.BUTTON_POSITIVE) {
+                // DB登録
+                try (var model = new CategoryModel(getActivity())) {
+                    model.create(new Category(0, _categoryName));
+                } catch (Exception e) {
+                    return;
+                }
+                // 登録完了ダイアログを表示
+                var completeDialog = new CreateCategoryCompleteDialogFragment();
+                completeDialog.show(getActivity().getSupportFragmentManager(), "CreateCategoryCompleteDialogFragment");
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/re_kid/lis/correctratelog/obj/Category.java
@@ -0,0 +1,52 @@
+package com.re_kid.lis.correctratelog.obj;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+public class Category implements Parcelable {
+    private final int id;
+    private final String name;
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    protected Category(Parcel in) {
+        id = in.readInt();
+        name = in.readString();
+    }
+
+    public static final Creator<Category> CREATOR = new Creator<>() {
+        @Override
+        public Category createFromParcel(Parcel in) {
+            return new Category(in);
+        }
+
+        @Override
+        public Category[] newArray(int size) {
+            return new Category[size];
+        }
+    };
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(name);
+    }
+}
