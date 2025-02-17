39061e893d19cc59b9e6349cf63bc3211a1e849f
Lis0715 <s.yamamoto.ad98@gmail.com>
2025-02-17 10:11:11 +0900

    バージョンアップ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.re_kid.lis.correctratelog"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.2.4"
+        versionCode = 5
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
