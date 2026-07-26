67675a2cd483070aa36eae1c3d3a50565c8825ac
Lis0715 <s.yamamoto.ad98@gmail.com>
2026-07-27 08:23:58 +0900

    バージョン番号を変更

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.re_kid.lis.correctratelog"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "v1.2.5"
+        versionCode = 14
+        versionName = "v1.2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
